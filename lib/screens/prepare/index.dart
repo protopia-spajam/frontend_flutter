4b6fff83ae60b176d1bcbf6447367af671c3d289
たむらけいと <tamurakeito@chibikeinoMacBook-Air.local>
2022-10-16 09:01:50 +0900

    loading実装

--- lib/screens/prepare/index.dart
+++ lib/screens/prepare/index.dart
@@ -3,6 +3,7 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:spajam2022/components/button/index.dart';
 import 'package:spajam2022/screens/result/index.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class PrepareView extends StatefulWidget {
   @override
@@ -12,6 +13,7 @@ class PrepareView extends StatefulWidget {
 class _PrepareViewState extends State<PrepareView> {
   File? image;
   final picker = ImagePicker();
+  bool isVisible = false;
 
   Future getImageFromGallery() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -21,62 +23,88 @@ class _PrepareViewState extends State<PrepareView> {
     });
   }
 
-  void post() {
-    /* ボタンがタップされた時の処理 */
+  /* ボタンがタップされた時の処理 */
+  Future handleClick(String title, String imageURL, String message) async {
+    setState(() {
+      isVisible = true;
+    });
+    // 一旦3秒だけloading表示
+    return Future.delayed(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ResultView(
+            title,
+            imageURL,
+            message,
+          ),
+        ),
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
-      body: Center(
-          child: Column(
+      body: Stack(
         children: [
-          Spacer(),
-          InkWell(
-            child: men_women_select("男の子をえらぶ", Colors.blue),
-            onTap: () {
-              getImageFromGallery();
-            },
+          Column(
+            children: [
+              Spacer(),
+              InkWell(
+                child: men_women_select("男の子をえらぶ", Colors.blue),
+                onTap: () {
+                  getImageFromGallery();
+                },
+              ),
+              InkWell(
+                child: men_women_select("女の子をえらぶ", Colors.pink),
+                onTap: () {
+                  getImageFromGallery();
+                },
+              ),
+              InkWell(
+                onTap: () {
+                  handleClick(
+                    'ずーーーっと、なかよし☺︎',
+                    'assets/images/resultImage1.jpeg',
+                    '海デートに行ったときの写真！\nこの後はしゃぎ過ぎて、\nふたりでびしょびしょになったよね笑',
+                  );
+                },
+                onDoubleTap: () {
+                  handleClick(
+                    '制服の思い出🫶',
+                    'assets/images/resultImage2.jpeg',
+                    '学校帰りに撮ってもらった写真📷\n毎日一緒に帰るの、\n楽しかったなあ〜！',
+                  );
+                },
+                child: Button('思い出作成！'),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Button('もどる'),
+              ),
+              Spacer(),
+            ],
           ),
-          InkWell(
-            child: men_women_select("女の子をえらぶ", Colors.pink),
-            onTap: () {
-              getImageFromGallery();
-            },
+          Visibility(
+            visible: isVisible,
+            child: Container(
+              color: const Color(0x55000000),
+              child: Center(
+                child: LoadingAnimationWidget.inkDrop(
+                  //この部分
+                  color: Colors.white,
+                  size: 100,
+                ),
+              ),
+            ),
           ),
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ResultView(
-                            'ずーーーっと、なかよし☺︎',
-                            'assets/images/resultImage1.jpeg',
-                            '海デートに行ったときの写真！\nこの後はしゃぎ過ぎて、\nふたりでびしょびしょになったよね笑',
-                          )));
-            },
-            onDoubleTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ResultView(
-                            '制服の思い出🫶',
-                            'assets/images/resultImage2.jpeg',
-                            '学校帰りに撮ってもらった写真📷\n毎日一緒に帰るの、\n楽しかったなあ〜！',
-                          )));
-            },
-            child: Button('思い出作成！'),
-          ),
-          InkWell(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Button('もどる'),
-          ),
-          Spacer(),
         ],
-      )),
+      ),
     );
   }
 }
