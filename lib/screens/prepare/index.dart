3632d6691ca7578618cb73b32a96b36352e1e5aa
たむらけいと <63249584+tamurakeito@users.noreply.github.com>
2022-10-16 12:01:23 +0900

    Revert "Revert "写真機能追加，コメント (#6)" (#7)" (#8)
    This reverts commit f795d0b9681ce7e91851dabc5780fcc6d38cdc53.

--- lib/screens/prepare/index.dart
+++ lib/screens/prepare/index.dart
@@ -1,3 +1,7 @@
+///PrepareView
+///変換したい画像(男，女)を選択する画面
+///ボタンを押すと変換後画面に遷移する
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
@@ -12,14 +16,24 @@ class PrepareView extends StatefulWidget {
 
 class _PrepareViewState extends State<PrepareView> {
   File? image;
+  File? image2;
   final picker = ImagePicker();
   bool isVisible = false;
 
+  //デバイスから画像を取得する（男の画像）
   Future getImageFromGallery() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
     setState(() {
       image = File(pickedFile!.path);
+    });
+  }
+  //デバイスから画像を取得する（女の画像）
+  Future getImageFromGallery2() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      image2 = File(pickedFile!.path);
     });
   }
 
@@ -49,140 +63,142 @@ class _PrepareViewState extends State<PrepareView> {
       backgroundColor: Colors.grey.shade100,
       body: Stack(
         children: [
-          Column(
-            children: [
-              const Spacer(),
-              Center(
-                child: Container(
-                  margin: const EdgeInsets.only(
-                    top: 30,
-                    right: 20,
-                    bottom: 30,
-                    left: 20,
-                  ),
-                  child: const Text(
-                    '新しく作る思い出の登場人物を設定してください！',
-                    style: TextStyle(
-                      fontFamily: 'AniFont',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Color(0xFF212121),
+          Spacer(),
+          InkWell(
+            child: SizedBox(
+              width: 500,
+              height: image==null ? 180 : 220,
+              // const(コンパイル定数)を定義できる
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                color: Colors.blue.shade100, // Cardの背景色
+                margin: const EdgeInsets.only(
+                  right: 30,
+                  left: 30,
+                  bottom: 40,
+                ), // Cardの外側の余白を設定するオプション
+                elevation: 2, // 影の離れ具合を調整するオプション
+                shadowColor: Colors.black, // 影の色を設定するオプション
+
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 40, bottom: 10),
+                      child: Text("男の子をえらぶ",
+                          style: TextStyle(
+                            fontFamily: 'AniFont',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.grey.shade800,
+                          )),
                     ),
-                  ),
+                    Container(
+                        width: 120,
+                        child: image == null
+                            ? SizedBox(
+                          height: 46,
+                          width: 46,
+                          child: Container(
+                            padding: EdgeInsets.zero,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(color: Colors.white, width: 2),
+                              color: Colors.grey.shade100,
+                            ),
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        )
+                            : Image.file(image!)
+                    ),
+
+                  ],
                 ),
               ),
-              InkWell(
-                child: men_women_select("男の子をえらぶ", Colors.blue),
-                onTap: () {
-                  getImageFromGallery();
-                },
+            ),
+            onTap: () {
+              getImageFromGallery();
+            },
+          ),
+
+          InkWell(
+            child: SizedBox(
+              width: 500,
+              height: image2==null ? 180 : 220,
+              // const(コンパイル定数)を定義できる
+              child: Card(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                color: Colors.pink.shade100, // Cardの背景色
+                margin: const EdgeInsets.only(
+                  right: 30,
+                  left: 30,
+                  bottom: 40,
+                ), // Cardの外側の余白を設定するオプション
+                elevation: 2, // 影の離れ具合を調整するオプション
+                shadowColor: Colors.black, // 影の色を設定するオプション
+
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 40, bottom: 10),
+                      child: Text("女の子をえらぶ",
+                          style: TextStyle(
+                            fontFamily: 'AniFont',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.grey.shade800,
+                          )),
+                    ),
+                    Container(
+                        width: 120,
+                        child: image2 == null
+                            ? SizedBox(
+                          height: 46,
+                          width: 46,
+                          child: Container(
+                            padding: EdgeInsets.zero,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(color: Colors.white, width: 2),
+                              color: Colors.grey.shade100,
+                            ),
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        )
+                            : Image.file(image2!)
+                    ),
+
+                  ],
+                ),
               ),
-              InkWell(
-                child: men_women_select("女の子をえらぶ", Colors.pink),
-                onTap: () {
-                  getImageFromGallery();
-                },
-              ),
-              InkWell(
-                onTap: () {
+            ),
+            onTap: () {
+              getImageFromGallery2();
+            },
+          ),
+
+          InkWell(
+            onTap: () {
                   handleClick(
                     'ずーーーっと、なかよし☺︎',
                     'assets/images/resultImage1.jpeg',
                     '海デートに行ったときの写真！\nこの後はしゃぎ過ぎて、\nふたりでびしょびしょになったよね笑',
                   );
-                },
-                onDoubleTap: () {
+            },
+            onDoubleTap: () {
                   handleClick(
                     '制服の思い出🫶',
                     'assets/images/resultImage2.jpeg',
                     '学校帰りに撮ってもらった写真📷\n毎日一緒に帰るの、\n楽しかったなあ〜！',
                   );
-                },
-                child: Button('思い出作成！'),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: Button('もどる'),
-              ),
-              const Spacer(),
-            ],
-          ),
-          Visibility(
-            visible: isVisible,
-            child: Container(
-              color: const Color(0x55000000),
-              child: Center(
-                child: LoadingAnimationWidget.inkDrop(
-                  //この部分
-                  color: Colors.white,
-                  size: 100,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+            },
+            child: Button('思い出作成！'),
     );
   }
 }
 
-class men_women_select extends StatelessWidget {
-  String description = "";
-  MaterialColor color = Colors.grey;
-  men_women_select(String description, MaterialColor color) {
-    this.description = description;
-    this.color = color;
-  }
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 500,
-      height: 180,
-      // const(コンパイル定数)を定義できる
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        color: color.shade100, // Cardの背景色
-        margin: const EdgeInsets.only(
-          right: 30,
-          left: 30,
-          bottom: 40,
-        ), // Cardの外側の余白を設定するオプション
-        elevation: 2, // 影の離れ具合を調整するオプション
-        shadowColor: Colors.black, // 影の色を設定するオプション
-
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(top: 40, bottom: 10),
-              child: Text("$description",
-                  style: TextStyle(
-                    fontFamily: 'AniFont',
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.grey.shade900,
-                  )),
-            ),
-            SizedBox(
-              height: 46,
-              width: 46,
-              child: Container(
-                padding: EdgeInsets.zero,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  border: Border.all(color: Colors.white, width: 2),
-                  color: Colors.grey.shade100,
-                ),
-                child: Icon(
-                  Icons.camera_alt_outlined,
-                  color: Colors.grey.shade500,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
